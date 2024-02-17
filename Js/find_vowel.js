5d0ec771fbc512deda820a40efeb10b71ce4ea5f
JD <137084651+jayududakiya@users.noreply.github.com>
2024-02-17 18:01:32 +0530

    17/02/2024

--- Js/find_vowel.js
+++ Js/find_vowel.js
@@ -1,3 +1,25 @@
 {
-    
+    let vowel = ['a' , 'e' , 'i' , 'o' ,'u'];
+    let Names = prompt("Enter word :","");
+    let vowel_count = 0;
+    let isVowel = "";
+    let i = 0;
+    console.log("in this word are Vowel is :");
+    while(i < Names.length)
+    {
+        let j = 0;
+        while(j < Names.length)
+        {
+            if(vowel[i] == Names[j])
+            {
+                isVowel += vowel[i]+ " ";
+                vowel_count = vowel_count + 1;
+            }
+            j++;
+        }
+        i++;
+    }
+
+    console.log(isVowel);
+    console.log("this is Vowel Count : " ,vowel_count);
 }
