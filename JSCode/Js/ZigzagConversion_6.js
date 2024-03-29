fd40125ac9804c3308d288ee2462271b54dc8429
JD <137084651+jayududakiya@users.noreply.github.com>
2024-03-29 18:58:19 +0530

    Questions-1,2

--- JSCode/Js/ZigzagConversion_6.js
+++ JSCode/Js/ZigzagConversion_6.js
@@ -30,94 +30,59 @@ Output: "A"
  
 */
 
+// let s = "PAYPALISHIRING";
+// console.log(s);
+// let Rownum = 3 ;
+// let steps = (Rownum * 2 ) - 2 ;
 
-let s = "PAYPALISHIRING";
-console.log(s);
-let Rownum = 3 ;
-let steps = (Rownum * 2 ) - 2 ;
+// let mainStr = ["","",""];
 
-let mainStr = ["","",""];
-
-for(let i = 0 ; i < s.length ; i++){
-    if(i%4 == 0){
-        mainStr[0] += s[i] + " ";
-    }else if(i%2== 0){
-        mainStr[2] += s[i] + " ";
-    }else{
-        if(i > 1){
-            mainStr[1] += s[i-2] + " ";
-        }else{
-            mainStr[1] += s[i+1] + " ";
-        }
-        // console.log(i-1);
-    }
-
-}
-
-console.log(mainStr.join(""));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// {    
-//     let str = "PAYPALISHIRING";
-//     let x = str.split("");
-//     let X2 = " ";
-//     // let strX = [];
-//     for(i = 1 ; i < str.length ; i++){
-//         if(i %3 == 0){
-//             x.splice(i,0," ")
+// for(let i = 0 ; i < s.length ; i++){
+//     if(i%4 == 0){
+//         mainStr[0] += s[i] + " ";
+//     }else if(i%2== 0){
+//         mainStr[2] += s[i] + " ";
+//     }else{
+//         if(i > 1){
+//             mainStr[1] += s[i-2] + " ";
+//         }else{
+//             mainStr[1] += s[i+1] + " ";
 //         }
+//         // console.log(i-1);
 //     }
-
-//     for(let j = 0 ; j <= x.length ; j++){
-//         if(X2 != 5){
-//             X2 = x.indexOf(" ");
-//             X2+=2
-//             x.splice(X2,0," ")
-//         }
-//     }
-    
-    
-    
-//     // x.splice(3,0," ");
-//     // x.splice(5,0," ");
-    
-//     // x.splice(9,0," ");
-//     // x.splice(11,0," ");
-    
-//     // x.splice(15,0," ");
-//     // x.splice(17,0," ");
-    
-    
-//     console.log(X2);
-//     console.log(x);
 
 // }
 
+// console.log(mainStr.join(""));
 
+function convert(s, numRows) {
+  if (numRows === 1) {
+    return s;
+  }
 
-    
-// // function convert (s) {
-//     //     let str = s;
-    //     let x = "" , y = "";
-    //     for(let i = 0 ; i <= s.length ; i++){
-    //         // if(i != s.length){
-    //         if(i % 3 == 0){
-    //             x += s.slice(i,i+3); 
-    //         }else{
-    //             x += s[i] = "0";
-    //         }
-    //     }
-    //     console.log(">",x);
-    // }; 
+  let mainStr = "";
+
+  const Steps = 2 * numRows - 2;
+
+  for (let i = 0; i < numRows; i++) {
+
+    for (let j = 0; j + i < s.length; j += Steps) {
+
+      mainStr += s[j + i]; // j++ + 0 , j++ + 1 , j++ + 2  
+
+      if (i !== 0 && i !== numRows - 1 && j + Steps - i < s.length) {
+
+        mainStr += s[j + Steps - i];
+
+      }
+
+    }
+
+  }
+
+  return mainStr;
+}
+
+let Ans = convert("PAYPALISHIRING",1);
+console.log(Ans);
+
